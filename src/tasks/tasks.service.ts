f3bb982fbb81dd76756c4b29dd017ff1810d8a8d
akwanmaroso <akwanmaroso@gmail.com>
2020-04-26 13:44:50 +0800

    fix(tasks): fix add tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
-import * as uuid from 'uuid';
+import * as uuid from 'uuid/v1';
 
 @Injectable()
 export class TasksService {
